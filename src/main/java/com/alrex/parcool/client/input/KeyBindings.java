62cadc24cac877d8f7de9f455dea643e93a9ebe8
alRex-U <83890990+alrex-u@users.noreply.github.com>
2022-02-19 10:28:53 +0900

    fix an issue of #14 -changed some default keybinding

--- src/main/java/com/alrex/parcool/client/input/KeyBindings.java
+++ src/main/java/com/alrex/parcool/client/input/KeyBindings.java
@@ -3,6 +3,7 @@ package com.alrex.parcool.client.input;
 import net.minecraft.client.GameSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,10 +15,10 @@ import org.lwjgl.glfw.GLFW;
 public class KeyBindings {
 	private static final GameSettings settings = Minecraft.getInstance().gameSettings;
 	private static final KeyBinding keyBindCrawl = new KeyBinding("key.crawl.description", GLFW.GLFW_KEY_C, "key.categories.movement");
-	private static final KeyBinding keyBindGrabWall = new KeyBinding("key.grab.description", GLFW.GLFW_MOUSE_BUTTON_RIGHT, "key.categories.movement");
+	private static final KeyBinding keyBindGrabWall = new KeyBinding("key.grab.description", InputMappings.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_RIGHT, "key.categories.movement");
 	private static final KeyBinding keyBindRoll = new KeyBinding("key.roll.description", GLFW.GLFW_KEY_C, "key.categories.movement");
 	private static final KeyBinding keyBindFastRunning = new KeyBinding("key.fastrunning.description", GLFW.GLFW_KEY_LEFT_CONTROL, "key.categories.movement");
-	private static final KeyBinding keyBindDodge = new KeyBinding("key.dodge.description", GLFW.GLFW_KEY_DELETE, "key.categories.movement");
+	private static final KeyBinding keyBindDodge = new KeyBinding("key.dodge.description", GLFW.GLFW_KEY_R, "key.categories.movement");
 	private static final KeyBinding keyBindOpenSettings = new KeyBinding("key.parcool.setting.open", GLFW.GLFW_KEY_P, "key.categories.parcool");
 
 	public static KeyBinding getKeySprint() {
