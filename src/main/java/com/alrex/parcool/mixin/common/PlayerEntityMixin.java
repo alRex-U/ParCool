54dcaf89113ceb157d6c372374a76679490215ca
alRex-U <83890990+alrex-u@users.noreply.github.com>
2022-08-16 10:50:13 +0900

    merge PlayerEntityMixins

--- src/main/java/com/alrex/parcool/mixin/common/PlayerEntityMixin.java
+++ src/main/java/com/alrex/parcool/mixin/common/PlayerEntityMixin.java
@@ -1,7 +1,8 @@
-package com.alrex.parcool.mixin;
+package com.alrex.parcool.mixin.common;
 
 import com.alrex.parcool.ParCoolConfig;
 import com.alrex.parcool.client.input.KeyBindings;
+import com.alrex.parcool.common.capability.Parkourability;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerAbilities;
@@ -14,16 +15,28 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
-	@Shadow
-	@Final
-	public PlayerAbilities abilities;
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> p_i48577_1_, World p_i48577_2_) {
 		super(p_i48577_1_, p_i48577_2_);
 	}
+
+	@Inject(method = "tryToStartFallFlying", at = @At("HEAD"), cancellable = true)
+	public void onTryToStartFallFlying(CallbackInfoReturnable<Boolean> cir) {
+		PlayerEntity player = (PlayerEntity) (Object) this;
+		Parkourability parkourability = Parkourability.get(player);
+		if (parkourability != null && parkourability.getClingToCliff().isCling()) {
+			cir.setReturnValue(false);
+			cir.cancel();
+		}
+	}
+
+	@Shadow
+	@Final
+	public PlayerAbilities abilities;
 
 	@Inject(method = "travel", at = @At("TAIL"))
 	public void onTravel(Vector3d p_213352_1_, CallbackInfo ci) {
