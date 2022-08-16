af65281ffec3a3bd7822cbd4933cce39feca83d8
alRex-U <83890990+alrex-u@users.noreply.github.com>
2022-08-16 17:16:32 +0900

    do #80 https://github.com/alRex-U/ParCool/issues/80#issuecomment-1145506370

--- src/main/java/com/alrex/parcool/client/input/KeyBindings.java
+++ src/main/java/com/alrex/parcool/client/input/KeyBindings.java
@@ -18,7 +18,7 @@ public class KeyBindings {
 	private static final KeyBinding keyBindGrabWall = new KeyBinding("key.grab.description", InputMappings.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_RIGHT, "key.categories.parcool");
 	private static final KeyBinding keyBindBreakfall = new KeyBinding("key.breakfall.description", GLFW.GLFW_KEY_C, "key.categories.parcool");
 	private static final KeyBinding keyBindFastRunning = new KeyBinding("key.fastrunning.description", GLFW.GLFW_KEY_LEFT_CONTROL, "key.categories.parcool");
-	private static final KeyBinding keyBindVault = new KeyBinding("key.vaultdescription", GLFW.GLFW_KEY_LEFT_CONTROL, "key.categories.parcool");
+	private static final KeyBinding keyBindVault = new KeyBinding("key.vaultdescription", InputMappings.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_RIGHT, "key.categories.parcool");
 	private static final KeyBinding keyBindDodge = new KeyBinding("key.dodge.description", GLFW.GLFW_KEY_R, "key.categories.parcool");
 	private static final KeyBinding keyBindWallSlide = new KeyBinding("key.wallslide.description", InputMappings.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_RIGHT, "key.categories.parcool");
 	private static final KeyBinding keyBindOpenSettings = new KeyBinding("key.parcool.setting.open", GLFW.GLFW_KEY_P, "key.categories.parcool");
