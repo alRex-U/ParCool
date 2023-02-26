77ff8d1ac83097b6278fbb6731c0357398b9a37f
alRex-U <83890990+alrex-u@users.noreply.github.com>
2023-02-26 16:46:03 +0900

    modify command arg suggestion

--- src/main/java/com/alrex/parcool/server/command/args/ActionArgumentType.java
+++ src/main/java/com/alrex/parcool/server/command/args/ActionArgumentType.java
@@ -14,6 +14,7 @@ import net.minecraft.util.text.TranslationTextComponent;
 
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class ActionArgumentType implements ArgumentType<Class<? extends Action>> {
 	@Override
@@ -29,7 +30,8 @@ public class ActionArgumentType implements ArgumentType<Class<? extends Action>>
 
 	@Override
 	public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-		for (String name : ActionList.NAMES) {
+		String remain = builder.getRemaining();
+		for (String name : ActionList.NAMES.stream().filter(it -> it.startsWith(remain)).collect(Collectors.toList())) {
 			builder.suggest(name);
 		}
 		return builder.buildFuture();
