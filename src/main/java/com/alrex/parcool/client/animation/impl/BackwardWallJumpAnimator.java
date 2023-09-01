2855a0b3cbf08fa46b1f83d19ee7443eb4cea98a
alRex-U <83890990+alrex-u@users.noreply.github.com>
2023-09-01 22:01:26 +0900

    fix BackwardWallJumpAnimator's camera animation not disabled

--- src/main/java/com/alrex/parcool/client/animation/impl/BackwardWallJumpAnimator.java
+++ src/main/java/com/alrex/parcool/client/animation/impl/BackwardWallJumpAnimator.java
@@ -114,7 +114,7 @@ public class BackwardWallJumpAnimator extends Animator {
 	public void onCameraSetUp(EntityViewRenderEvent.CameraSetup event, PlayerEntity clientPlayer, Parkourability parkourability) {
 		if (!(clientPlayer.isLocalPlayer() &&
 				Minecraft.getInstance().options.getCameraType().isFirstPerson() &&
-				ParCoolConfig.Client.Booleans.EnableWallJumpBackward.get()
+				ParCoolConfig.Client.Booleans.EnableCameraAnimationOfBackWallJump.get()
 		)) return;
 		float phase = (float) ((getTick() + event.getRenderPartialTicks()) / maxTick);
 		float factor = angleFactor(phase);
