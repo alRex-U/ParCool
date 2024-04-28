06058d2ebac06074a293e15ec021da2bbe566609
alRex-U <83890990+alrex-u@users.noreply.github.com>
2024-04-28 12:23:11 +0900

    bugfix : command permission is at incorrect pos

--- src/main/java/com/alrex/parcool/server/command/impl/ControlLimitationCommand.java
+++ src/main/java/com/alrex/parcool/server/command/impl/ControlLimitationCommand.java
@@ -225,10 +225,10 @@ public class ControlLimitationCommand {
                 )
                 .then(Commands
                         .literal("enable")
+                        .requires(commandSource -> commandSource.hasPermission(2))
                         .then(
                                 getLimitationByNameCommands(true, (it) -> {
                                     it
-                                            .requires(commandSource -> commandSource.hasPermission(2))
                                             .executes((context) -> enableLimitation(context, true, true));
                                     return it;
                                 })
@@ -244,10 +244,10 @@ public class ControlLimitationCommand {
                 )
                 .then(Commands
                         .literal("disable")
+                        .requires(commandSource -> commandSource.hasPermission(2))
                         .then(
                                 getLimitationByNameCommands(true, (it) -> {
                                     it
-                                            .requires(commandSource -> commandSource.hasPermission(2))
                                             .executes((context) -> disableLimitation(context, true, true));
                                     return it;
                                 })
@@ -255,7 +255,6 @@ public class ControlLimitationCommand {
                         .then(
                                 getIndividualLimitationCommands(true, (it) -> {
                                     it
-                                            .requires(commandSource -> commandSource.hasPermission(2))
                                             .executes((context) -> disableLimitation(context, false, true));
                                     return it;
                                 })
@@ -263,9 +262,9 @@ public class ControlLimitationCommand {
                 )
                 .then(Commands
                         .literal("delete")
+                        .requires(commandSource -> commandSource.hasPermission(2))
                         .then(Commands
                                 .argument(ARGS_NAME_LIMITATION_ID, LimitationIDArgumentType.limitation())
-                                .requires(commandSource -> commandSource.hasPermission(2))
                                 .executes(ControlLimitationCommand::deleteLimitation)
                         )
                 );
