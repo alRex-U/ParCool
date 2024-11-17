5d308d758844c496d087fc60e341cc67ccf220c8
alRex-U <83890990+alRex-U@users.noreply.github.com>
2024-11-18 01:36:41 +0900

    apply changes to 1.19.2

--- src/main/java/com/alrex/parcool/mixin/client/LocalPlayerMixin.java
+++ src/main/java/com/alrex/parcool/mixin/client/LocalPlayerMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.ProfilePublicKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
