8635c0f01b3cafb29c10b64dfb1ed138c406572e
alRex-U <83890990+alRex-U@users.noreply.github.com>
2025-04-13 22:54:32 +0900

    disable destroying blocks while HideInBlock

--- src/main/java/com/alrex/parcool/common/handlers/InputHandler.java
+++ src/main/java/com/alrex/parcool/common/handlers/InputHandler.java
@@ -3,6 +3,7 @@ package com.alrex.parcool.common.handlers;
 import com.alrex.parcool.client.input.KeyBindings;
 import com.alrex.parcool.common.action.impl.ClingToCliff;
 import com.alrex.parcool.common.action.impl.HideInBlock;
+import com.alrex.parcool.common.action.impl.RideZipline;
 import com.alrex.parcool.common.capability.Parkourability;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
@@ -12,17 +13,24 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class InputHandler {
     @SubscribeEvent
     public static void onInput(InputEvent.ClickInputEvent event) {
-        if (!event.getKeyBinding().getKey().equals(KeyBindings.getKeyGrabWall().getKey())) return;
         ClientPlayerEntity player = Minecraft.getInstance().player;
         if (player == null) return;
         Parkourability parkourability = Parkourability.get(player);
         if (parkourability == null) return;
         if (event.isUseItem() && parkourability.get(ClingToCliff.class).isDoing()) {
-            event.setSwingHand(false);
-            event.setCanceled(true);
+            if (event.getKeyBinding().getKey().equals(KeyBindings.getKeyGrabWall().getKey())) {
+                event.setSwingHand(false);
+                event.setCanceled(true);
+            }
         } else if (parkourability.get(HideInBlock.class).isDoing()) {
             event.setSwingHand(false);
             event.setCanceled(true);
+        }
+        if (event.isUseItem() && parkourability.get(RideZipline.class).isDoing()) {
+            if (event.getKeyBinding().getKey().equals(KeyBindings.getKeyBindRideZipline().getKey())) {
+                event.setSwingHand(false);
+                event.setCanceled(true);
+            }
         }
     }
 }
