d3e5ed1dec59f365cb6ab45676f6686fbeb904ce
alRex-U <83890990+alRex-U@users.noreply.github.com>
2025-05-03 20:21:37 +0900

    bugfix : incorrect RenderType is used

--- src/main/java/com/alrex/parcool/client/renderer/RenderTypes.java
+++ src/main/java/com/alrex/parcool/client/renderer/RenderTypes.java
@@ -7,10 +7,21 @@ import org.lwjgl.opengl.GL11;
 
 public class RenderTypes {
     public static final RenderType ZIPLINE_3D;
+    public static final RenderType ZIPLINE_2D;
 
     static {
+        ZIPLINE_2D = RenderType.create(
+                "zipline3d",
+                DefaultVertexFormats.POSITION_COLOR_LIGHTMAP,
+                GL11.GL_QUADS, 256,
+                RenderType.State.builder()
+                        .setTextureState(RenderState.NO_TEXTURE)
+                        .setCullState(RenderState.NO_CULL)
+                        .setLightmapState(RenderState.LIGHTMAP)
+                        .createCompositeState(false)
+        );
         ZIPLINE_3D = RenderType.create(
-                "zipline2d",
+                "zipline3d",
                 DefaultVertexFormats.POSITION_COLOR_LIGHTMAP,
                 GL11.GL_QUADS, 256,
                 RenderType.State.builder()
