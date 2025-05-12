5155dba7da38cd7c0fa24d0d643ca273b70b785c
Thiago Santos <tos.oliveira@gmail.com>
2025-05-12 07:18:18 -0300

    feat: adapting dodge to decoupled camera
    Making dodge action always be a forward roll
    in the direction the user inputed.
    The yRot of the player is also set make it
    face the right angle after rolling

--- src/main/java/com/alrex/parcool/extern/betterthirdperson/BetterThirdPersonManager.java
+++ src/main/java/com/alrex/parcool/extern/betterthirdperson/BetterThirdPersonManager.java
@@ -1,12 +1,10 @@
 package com.alrex.parcool.extern.betterthirdperson;
 
 import com.alrex.parcool.common.action.impl.Dodge;
+import com.alrex.parcool.extern.AdditionalMods;
 import com.alrex.parcool.extern.ModManager;
-import com.alrex.parcool.utilities.MathUtil;
 import io.socol.betterthirdperson.BetterThirdPerson;
-import io.socol.betterthirdperson.api.CustomCamera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
 
 public class BetterThirdPersonManager extends ModManager {
     public BetterThirdPersonManager() {
@@ -14,24 +12,12 @@ public class BetterThirdPersonManager extends ModManager {
     }
 
     public Dodge.DodgeDirection handleCustomCameraRotationForDodge(Dodge.DodgeDirection direction) {
-        if (!isInstalled()) return direction;
-        if (!Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
-            ClientPlayerEntity player = Minecraft.getInstance().player;
-            if (player == null) return direction;
-            if (!BetterThirdPerson.getCameraManager().hasCustomCamera()) return direction;
-            CustomCamera camera = BetterThirdPerson.getCameraManager().getCustomCamera();
-            float yaw = MathUtil.normalizeDegree(camera.getPlayerRotation().getYaw() - camera.getCameraRotation().getYaw());
-            float yawAbs = Math.abs(yaw);
-            if (yawAbs < 45) {
-                return direction;
-            } else if (yawAbs > 135) {
-                return direction.inverse();
-            } else if (yaw < 0) {
-                return direction.right();
-            } else {
-                return direction.left();
-            }
-        }
-        return direction;
+        return isCameraDecoupled() ? Dodge.DodgeDirection.Front : direction;
+    }
+
+    public boolean isCameraDecoupled() {
+        return isInstalled()
+            && !Minecraft.getInstance().options.getCameraType().isFirstPerson()
+            && BetterThirdPerson.getCameraManager().hasCustomCamera();
     }
 }
