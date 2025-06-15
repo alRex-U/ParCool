ac675eba30c82e3d675dde153bcc5c10db3b67a8
alRex-U <83890990+alRex-U@users.noreply.github.com>
2025-06-15 12:44:17 +0900

    add pole climbable tag

--- src/main/java/com/alrex/parcool/common/tags/BlockTags.java
+++ src/main/java/com/alrex/parcool/common/tags/BlockTags.java
@@ -5,4 +5,5 @@ import net.minecraft.util.ResourceLocation;
 
 public class BlockTags {
     public static final ResourceLocation HIDE_ABLE = new ResourceLocation(ParCool.MOD_ID, "hide_able");
+    public static final ResourceLocation POLE_CLIMBABLE = new ResourceLocation(ParCool.MOD_ID, "pole_climbable");
 }
