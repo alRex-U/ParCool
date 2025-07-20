d449e551ef826865fb15d03e83ed88ff220bcd77
alRex-U <83890990+alRex-U@users.noreply.github.com>
2025-07-20 16:29:46 +0900

    apply changes to 1.21

--- src/main/java/com/alrex/parcool/common/action/InstantAction.java
+++ src/main/java/com/alrex/parcool/common/action/InstantAction.java
@@ -1,29 +1,28 @@
 package com.alrex.parcool.common.action;
 
-import com.alrex.parcool.common.capability.IStamina;
-import com.alrex.parcool.common.capability.Parkourability;
+import com.alrex.parcool.common.attachment.common.Parkourability;
 import net.minecraft.world.entity.player.Player;
 
 public abstract class InstantAction extends Action {
     @Override
-    public final boolean canContinue(Player player, Parkourability parkourability, IStamina stamina) {
+    public final boolean canContinue(Player player, Parkourability parkourability) {
         return false;
     }
 
     @Override
-    public final void onWorkingTick(Player player, Parkourability parkourability, IStamina stamina) {
+    public final void onWorkingTick(Player player, Parkourability parkourability) {
     }
 
     @Override
-    public final void onWorkingTickInClient(Player player, Parkourability parkourability, IStamina stamina) {
+    public final void onWorkingTickInClient(Player player, Parkourability parkourability) {
     }
 
     @Override
-    public final void onWorkingTickInLocalClient(Player player, Parkourability parkourability, IStamina stamina) {
+    public final void onWorkingTickInLocalClient(Player player, Parkourability parkourability) {
     }
 
     @Override
-    public final void onWorkingTickInServer(Player player, Parkourability parkourability, IStamina stamina) {
+    public final void onWorkingTickInServer(Player player, Parkourability parkourability) {
     }
 
     @Override
--- src/main/java/com/alrex/parcool/common/action/instant/StartSwimByCrawl.java
+++ src/main/java/com/alrex/parcool/common/action/instant/StartSwimByCrawl.java
@@ -3,15 +3,14 @@ package com.alrex.parcool.common.action.instant;
 import com.alrex.parcool.client.input.KeyRecorder;
 import com.alrex.parcool.common.action.InstantAction;
 import com.alrex.parcool.common.action.StaminaConsumeTiming;
-import com.alrex.parcool.common.capability.IStamina;
-import com.alrex.parcool.common.capability.Parkourability;
+import com.alrex.parcool.common.attachment.common.Parkourability;
 import net.minecraft.world.entity.player.Player;
 
 import java.nio.ByteBuffer;
 
 public class StartSwimByCrawl extends InstantAction {
     @Override
-    public boolean canStart(Player player, Parkourability parkourability, IStamina stamina, ByteBuffer startInfo) {
+    public boolean canStart(Player player, Parkourability parkourability, ByteBuffer startInfo) {
         return player.isInWaterOrBubble()
                 && !player.isSwimming()
                 && KeyRecorder.keyCrawlState.isPressed();
