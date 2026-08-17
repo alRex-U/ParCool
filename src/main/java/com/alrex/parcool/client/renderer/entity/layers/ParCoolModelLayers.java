c897c540c399c71535845888de4050901b8603d8
alRex-U <83890990+alRex-U@users.noreply.github.com>
2026-08-17 16:46:07 +0900

    fix equipment model layer name

--- src/main/java/com/alrex/parcool/client/renderer/entity/layers/ParCoolModelLayers.java
+++ src/main/java/com/alrex/parcool/client/renderer/entity/layers/ParCoolModelLayers.java
@@ -6,21 +6,22 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 
 @OnlyIn(Dist.CLIENT)
 public class ParCoolModelLayers {
-    public static final ModelLayerLocation INNER_EQUIPMENT = new ModelLayerLocation(ParCool.resourceLocation("equipment"), "equipment");
-    public static final ModelLayerLocation INNER_EQUIPMENT_SLIM = new ModelLayerLocation(ParCool.resourceLocation("equipment"), "equipment");
-    public static final ModelLayerLocation OUTER_EQUIPMENT = new ModelLayerLocation(ParCool.resourceLocation("equipment"), "equipment");
-    public static final ModelLayerLocation OUTER_EQUIPMENT_SLIM = new ModelLayerLocation(ParCool.resourceLocation("equipment"), "equipment");
+    public static final ModelLayerLocation INNER_EQUIPMENT = new ModelLayerLocation(ResourceLocation.withDefaultNamespace("player"), "parcool.equipment_in");
+    public static final ModelLayerLocation INNER_EQUIPMENT_SLIM = new ModelLayerLocation(ResourceLocation.withDefaultNamespace("player_slim"), "parcool.equipment_in");
+    public static final ModelLayerLocation OUTER_EQUIPMENT = new ModelLayerLocation(ResourceLocation.withDefaultNamespace("player"), "parcool.equipment_out");
+    public static final ModelLayerLocation OUTER_EQUIPMENT_SLIM = new ModelLayerLocation(ResourceLocation.withDefaultNamespace("player_slim"), "parcool.equipment_out");
     public static final ModelLayerLocation GRAPPLE_TIP = new ModelLayerLocation(ParCool.resourceLocation("grapple_tip"), "main");
 
     public static void register(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        var innerEquipmentDefinition = LayerDefinition.create(HumanoidModel.createMesh(new CubeDeformation(0.6f), 0.0F), 64, 32);
-        var outerEquipmentDefinition = LayerDefinition.create(HumanoidModel.createMesh(new CubeDeformation(0.1f), 0.0F), 64, 32);
+        var innerEquipmentDefinition = LayerDefinition.create(HumanoidModel.createMesh(new CubeDeformation(0.1f), 0.0F), 64, 32);
+        var outerEquipmentDefinition = LayerDefinition.create(HumanoidModel.createMesh(new CubeDeformation(0.6f), 0.0F), 64, 32);
         event.registerLayerDefinition(INNER_EQUIPMENT, () -> innerEquipmentDefinition);
         event.registerLayerDefinition(INNER_EQUIPMENT_SLIM, () -> innerEquipmentDefinition);
         event.registerLayerDefinition(OUTER_EQUIPMENT, () -> outerEquipmentDefinition);
