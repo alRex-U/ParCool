bcf17b4ef5f78a3ccd608220e79b28e656db77ca
alRex-U <83890990+alRex-U@users.noreply.github.com>
2026-09-16 23:51:46 +0900

    apply changes to 1.21.1

--- src/main/java/com/alrex/parcool/common/action/impl/LongJump.java
+++ src/main/java/com/alrex/parcool/common/action/impl/LongJump.java
@@ -18,8 +18,8 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 
 import java.util.List;
 
